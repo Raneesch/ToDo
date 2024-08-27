3452f87c66a46732a992b166070669e333c1b491
Raneesch <raneeskhadarch@gmail.com>
2024-08-27 22:55:07 +0530

    mark as done function added

--- todo/views.py
+++ todo/views.py
@@ -1,5 +1,5 @@
 from django.http import HttpResponse
-from django.shortcuts import redirect, render
+from django.shortcuts import get_object_or_404, redirect, render
 
 from todo.models import Task
 
@@ -13,3 +13,9 @@ def addTask(requests):
 #     update = requests.POST['task']
 #     Task.objects.delete(task=update)
 #     return redirect('home')
+
+def mark_as_done(requests,pk):
+    task = get_object_or_404(Task, pk=pk)
+    task.is_completed = True
+    task.save()
+    return redirect('home')
--- todo_main/views.py
+++ todo_main/views.py
@@ -6,9 +6,11 @@ from todo.models import Task
 def home(request):  
     
     task=Task.objects.filter(is_completed=False)
+    completed_task = Task.objects.filter(is_completed = True)
     
     context = {
-        'task' : task
+        'task' : task,
+        'completed_task' : completed_task
     }
     
     return render(request,'home-todo.html',context)
